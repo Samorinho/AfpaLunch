247fe07585d9e129546a0362c752508305462e0a
johandavid@me.com <johandavid@me.com>
2020-03-12 17:22:19 +0100

    Modal Restaurateur OK

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -15,6 +15,9 @@ namespace AfpaLunch
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/blabla").Include(
+                        "~/Scripts/blabla.js"));
+
             // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
             // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Scripts/blabla.js
@@ -0,0 +1,19 @@
+﻿
+$("#envoi").css("display", "none");
+$("#retour").css("display", "none");
+
+$("#continuer").click(function () {
+    $("#debut").css("display", "none");
+    $("#suite").css("display", "block");
+    $("#retour").css("display", "block");
+    $("#continuer").css("display", "none");
+    $("#envoi").css("display", "block");
+});
+
+$("#retour").click(function () {
+    $("#debut").css("display", "block");
+    $("#suite").css("display", "none");
+    $("#retour").css("display", "none");
+    $("#continuer").css("display", "block");
+    $("#envoi").css("display", "none");
+});
